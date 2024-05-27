20ee160916914b9de654902dfef41878c751ce19
DanteSwagKid69 <dante.vonmatern@elev.ga.ntig.se>
2024-05-27 16:50:52 +0200

    skriver som i e labben

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,42 @@
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
+import java.net.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        ServerSocket server = null;
+        Socket client;
 
+        // Default port number we are going to use
+        int portNumber = 50000;
+        if (args.length >= 1) {
+            portNumber = Integer.parseInt(args[0]);
+        }
+
+        // Create server side socket
+        try {
+            server = new ServerSocket(portNumber);
+        } catch(IOException ie) {
+            System.out.println("Canot open socket." + ie);
+            System.exit(1);
+        }
+        System.out.println("ServerSocket is created " + server);
+
+        // Wait for the data from the client and reply
+        while(true) {
+            try {
+                // Listens for a connection to be made to
+                // This socket and accepts it. The methods block until
+                // a connection is made
+                System.out.println("Waiting for connect request...");
+                client = server.accept();
+
+                System.out.println("Connect request is accepted...");
+                String clientHost = client.getInetAddress().getHostName();
+                int clientPort = client.getPort();
+                System.out.println("Client host = " + clientHost + " Client port = " + clientPort);
+            }
+        }
     }
 
     private static String allMethodDependingOnOperatorSign(String msg) {
