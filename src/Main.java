98e8f2afd295329f861211bb25746f0bc7fd75f1
DanteSwagKid69 <dante.vonmatern@elev.ga.ntig.se>
2024-05-27 16:44:29 +0200

    Börjar om genom att kopiera e nivån

--- src/Main.java
+++ src/Main.java
@@ -2,57 +2,31 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+
 public class Main {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
-        // Default port number we are going to use
-        int portNumber = 50000;
-        if (args.length >= 1) {
-            portNumber = Integer.parseInt(args[0]);
-        }
 
-        // Create a MulticastSocket
-        MulticastSocket serverMulticastSocket = new MulticastSocket(portNumber);
-        System.out.println("MulticastSocket is created at port " + portNumber);
-
-        // Determine the IP adress of host, given the host name
-        InetAddress group = InetAddress.getByName("225.4.5.6");
-
-        // getByName- returns IP adress of given host
-        serverMulticastSocket.joinGroup(group);
-        System.out.println("joinGroup method is called...");
-
-        boolean infinite = true;
-
-        // Continually recevies date and prints them
-        while(infinite) {
-            byte[] buf= new byte[1024];
-            DatagramPacket data = new DatagramPacket(buf, buf.length);
-            serverMulticastSocket.receive(data);
-            String msg = new String(data.getData()).trim();
-
-            // Get the operator sign, for example + or -
-            callMethodDependingOnOperatorSign(msg);
-
-            System.out.println("Message received from client = " + msg);
-        }
-        serverMulticastSocket.close();
     }
 
-    static void callMethodDependingOnOperatorSign(String msg) {
+    private static String allMethodDependingOnOperatorSign(String msg) {
         String[] args = new String[2];
         if (msg.contains("+")) {
             args = msg.split("\\+");
             System.out.println(args[0] + " | " + args[1]);
 
+            return addition(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        } else {
+
+            return "NOLL";
         }
+
     }
 
+
     static String addition(int arg1, int arg2) {
-        String result = "";
-
-
-        return result;
+        int result = arg1 + arg2;
+        return String.valueOf(result);
     }
 
     static String subtraction(int arg1, int arg2) {
