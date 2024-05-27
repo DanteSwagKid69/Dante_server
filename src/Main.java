9156b30b5ec6945a9df25275d1ca234ce27b6e5a
DanteSwagKid69 <dante.vonmatern@elev.ga.ntig.se>
2024-05-27 17:25:02 +0200

    Nu fungerar allt

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import java.io.IOException;
+import javax.swing.*;
+import java.io.*;
 import java.net.*;
 
 public class Main {
@@ -35,20 +36,53 @@ public class Main {
                 String clientHost = client.getInetAddress().getHostName();
                 int clientPort = client.getPort();
                 System.out.println("Client host = " + clientHost + " Client port = " + clientPort);
+
+                // Read data from the client
+                InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+                String msgFromClient = br.readLine();
+                System.out.println("Message received from client = " + msgFromClient);
+
+                // Send response to client
+                if (msgFromClient != null && !msgFromClient.equalsIgnoreCase("Bye")) {
+
+
+
+                    OutputStream clientOut = client.getOutputStream();
+                    PrintWriter pw = new PrintWriter(clientOut, true);
+                    //String ansMsg = "Hello, " + msgFromClient;
+                    String ansMsg = callMethodDependingOnOperatorSign(msgFromClient);
+                    pw.println(ansMsg);
+                }
+
+                // Close the sockets
+                if (msgFromClient != null && msgFromClient.equalsIgnoreCase("Bye")) {
+                    server.close();
+                    client.close();
+                    break;
+                }
+            } catch (IOException ie) {
+                System.out.println("Could not connect to server" + ie);
             }
         }
     }
 
-    private static String allMethodDependingOnOperatorSign(String msg) {
+    private static String callMethodDependingOnOperatorSign(String msg) {
         String[] args = new String[2];
         if (msg.contains("+")) {
             args = msg.split("\\+");
-            System.out.println(args[0] + " | " + args[1]);
-
             return addition(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        } else if (msg.contains("-")) {
+            args = msg.split("-");
+            return subtraction(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        } else if (msg.contains("/")) {
+            args = msg.split("/");
+            return division(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        }else if (msg.contains("*")) {
+            args = msg.split("\\*");
+            return multiplication(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
         } else {
-
-            return "NOLL";
+            return "You did not enter a math problem";
         }
 
     }
@@ -60,24 +94,18 @@ public class Main {
     }
 
     static String subtraction(int arg1, int arg2) {
-        String result = "";
-
-
-        return result;
+        int result = arg1 - arg2;
+        return String.valueOf(result);
     }
 
     static String division(int arg1, int arg2) {
-        String result = "";
-
-
-        return result;
+        int result = arg1 / arg2;
+        return String.valueOf(result);
     }
 
     static String multiplication(int arg1, int arg2) {
-        String result = "";
-
-
-        return result;
+        int result = arg1 * arg2;
+        return String.valueOf(result);
     }
 
 
