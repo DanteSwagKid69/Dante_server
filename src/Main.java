50d83affe7635a3149391b035f0a7d97367783b9
DanteSwagKid69 <dante.vonmatern@elev.ga.ntig.se>
2024-05-27 16:01:38 +0200

    Copied the code from c nivå

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception{
+
+        // Default port number we are going to use
+        int portNumber = 50000;
+        if (args.length >= 1) {
+            portNumber = Integer.parseInt(args[0]);
+        }
+
+        // Create a MulticastSocket
+        MulticastSocket serverMulticastSocket = new MulticastSocket(portNumber);
+        System.out.println("MulticastSocket is created at port " + portNumber);
+
+        // Determine the IP adress of host, given the host name
+        InetAddress group = InetAddress.getByName("225.4.5.6");
+
+        // getByName- returns IP adress of given host
+        serverMulticastSocket.joinGroup(group);
+        System.out.println("joinGroup method is called...");
+
+        boolean infinite = true;
+
+        // Continually recevies date and prints them
+        while(infinite) {
+            byte[] buf= new byte[1024];
+            DatagramPacket data = new DatagramPacket(buf, buf.length);
+            serverMulticastSocket.receive(data);
+            String msg = new String(data.getData()).trim();
+            System.out.println("Message received from client = " + msg);
+        }
+        serverMulticastSocket.close();
     }
 }
